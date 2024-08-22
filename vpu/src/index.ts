830d0b220fc2a7ec31a8e9dd225e6d7a2e4c7451
Ludis <leulsegedmelaku1020@gmail.com>
2024-08-22 16:27:15 +0300

    chore: Add fluent-ffmpeg as a dependency and update package-lock.json and package.json

--- vpu/src/index.ts
+++ vpu/src/index.ts
@@ -1,12 +1,18 @@
-import express from "express"
+import express from "express";
+import Ffmpeg from "fluent-ffmpeg";
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+app.post("/process-video", (req, res) => {
+  const inputFilePath = req.body.inputFilePath;
+  const outputFilePath = req.body.outputFilePath;
+
+  if (!inputFilePath || !outputFilePath) {
+    res.status(400).send("Bad request: Missing file path. ");
+  }
+});
 
 app.listen(port, () => {
-    console.log("serving")
-})
+  console.log("serving");
+});
